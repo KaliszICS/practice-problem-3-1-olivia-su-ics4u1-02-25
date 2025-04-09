45b11866c86b9cc2bd3f604c3d2f82ea55b02575
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-04-09 05:09:30 +0000

    commit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,29 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int x) {
 
+		if (x < 0) {
+			x = -x;
+		}
+		if (x < 10) {
+			return x;
+		}
+		return (x % 10) + sumOfDigits(x / 10);
+		// x % 10, finds last digit by finding the remainder ex. 234 % 10 = 4; x / 10 finds the remaining digits ex. 234 / 10 = 23.4 ignore 4
+	}
+
+	public static int factorial(int n) {
+
+		if (n == 0) {
+			return 1;
+		}
+		if (n < 0) {
+			return 0;
+		}
+		return n * factorial(n -1);
+		// reduces n each time
+	}
+  	
 }
+
